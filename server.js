f23455571f037c41c801c33108f7b2c20cc9ee4d
Justin Kunz <Justin_Kunz@yahoo.com>
2019-01-20 16:36:59 -0600

    work you dick

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ var PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 require('./routes/apiRoutes')(app)
 
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(PORT)
 
